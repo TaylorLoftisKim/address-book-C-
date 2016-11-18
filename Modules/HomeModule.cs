39f3c527e37fc8842c3dd5f60a7fc5735677297d
Ada Lovelace and Taylor Loftis-Kim <ada+taylor@>
2016-11-18 09:09:32 -0800

    make additions to HomeModule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,7 +14,16 @@ namespace ContactList
       Get["/contacts/{id}"] = parameters =>
       {
         Dictionary<string, object> model = new Dictionary<string, object>();
-        var selectedContact
+        var selectedContact = Contact.Find(parameters.id);
+        var contactAddress = selectedContact,GetAddressMulti();
+        model.Add("artist", selectedContact);
+        model.Add("address", contactAddressMulti);
+        return View["view-all-cd.cshtml", model];
+      };
+      Get["/view-all-contacts"] = _ =>
+      {
+        var allContacts = Contacts.GetAll();
+        return View["view-all-contact.cshtml"]
       }
     }
   }
